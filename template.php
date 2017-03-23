b303340cb83066a266ff6bd08e8e7ac5782d40f9
dan12t3 <dan_12t3@hotmail.com>
2017-03-22 20:55:29 -0400

    update

---
+++ template.php
@@ -0,0 +1,54 @@
+<?php require("header.html"); ?>
+
+<div class="customContainer">
+    
+    <nav class="navbar navbar-default secondNav">
+
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#secondNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+    </div>
+    <div class="collapse navbar-collapse secondNavBody" id="secondNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Page 1</a></li>
+        <li><a href="#">Page 2</a></li> 
+        <li><a href="#">Page 3</a></li> 
+      </ul>
+      <ul class="nav navbar-right">
+        
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  
+</nav>
+    
+    
+    <div class="row">
+    <div class="col-lg-9">
+        
+        <div class="col-lg-6">
+        <button class="btn">Apply</button>
+        </div>
+        <div class="col-lg-6">
+        <button class="btn">Next Steps</button>
+        </div>
+        
+      
+    </div>
+    <div class="col-lg-3">
+        
+        Sidebar
+        </div>
+    
+    
+    </div>
+    
+    
+
+</div>
+
+<?php require("footer.html"); ?>
